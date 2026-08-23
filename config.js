97b2f3ff439b2676d51902baafb2f88f0f9917d0
nikhilrajv0025 <nikhilrajv0025@gmail.com>
2026-08-24 00:06:57 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ window.AHARAM_CONFIG = {
   whatsappNumber: "917448528521",
 
   // ===== ADD YOUR LINKS BELOW =====
-  swiggyLink: "",       // Example: "https://www.swiggy.com/city/..."
-  googleMapsLink: "",   // Example: "https://maps.app.goo.gl/..."
+  swiggyLink: "https://www.swiggy.com/menu/1393703?source=sharing",       // Example: "https://www.swiggy.com/city/..."
+  googleMapsLink: "https://www.google.com/maps/dir//AHARAM,+17,+7th+Cross+Rd,+LBS+Nagar,+Kaggadasapura,+Bengaluru,+Karnataka+560017/@12.9599633,77.6529331,14.1z/data=!4m8!4m7!1m0!1m5!1m1!1s0x3bae13003b0e9e45:0x7d623a4781058ce8!2m2!1d77.6710117!2d12.9692009?entry=ttu&g_ep=EgoyMDI2MDgxOS4wIKXMDSoASAFQAw%3D%3D",   // Example: "https://maps.app.goo.gl/..."
   instagramLink: "",    // Example: "https://instagram.com/aharam..."
   // =================================
 
