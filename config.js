29730491e2b8246c5bd070007f407bffc2e21829
nikhilrajv0025 <nikhilrajv0025@gmail.com>
2026-08-30 14:35:49 +0530

    image edit

--- config.js
+++ config.js
@@ -5,11 +5,11 @@ window.AHARAM_CONFIG = {
   whatsappNumber: "917448528521",
 
   // ===== ADD YOUR LINKS BELOW =====
-  swiggyLink: "",       // Example: "https://www.swiggy.com/city/..."
-  googleMapsLink: "",   // Example: "https://maps.app.goo.gl/..."
-  instagramLink: "",    // Example: "https://instagram.com/aharam..."
+  swiggyLink: "https://www.swiggy.com/menu/1393703?source=sharing",      
+  googleMapsLink: "https://www.google.com/maps/dir//AHARAM,+17,+7th+Cross+Rd,+LBS+Nagar,+Kaggadasapura,+Bengaluru,+Karnataka+560017/@12.9599633,77.6529331,14.1z/data=!4m8!4m7!1m0!1m5!1m1!1s0x3bae13003b0e9e45:0x7d623a4781058ce8!2m2!1d77.6710117!2d12.9692009?entry=ttu&g_ep=EgoyMDI2MDgxOS4wIKXMDSoASAFQAw%3D%3D",
+  instagramLink: "",    
   // =================================
 
   address: "17, 7th Cross Road, LBS Nagar, Kaggadasapura, Bengaluru",
-  openingHours: "ADD OPENING HOURS HERE", // Example: "Every day, 10:00 AM – 10:00 PM"
+  openingHours: "11am - 10pm", 
 };
